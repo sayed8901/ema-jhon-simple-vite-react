cb5eaf86526738493fae2e22d29c4a36d36089a7
sayed8901 <sayed91515@gmail.com>
2023-03-29 01:21:01 +0600

    cartAdd button created

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCoffee , faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import "./Product.css";
 
 const Product = (props) => {
-  console.log(props.product);
+  // console.log(props.product);
   const { img, name, price, ratings, seller, quantity } = props.product;
+
+  const handleAddToCart = props.handleAddToCart;
+
   return (
     <div className="product">
       <img src={img} alt="" />
@@ -11,9 +16,13 @@ const Product = (props) => {
         <h3 className="product-name">{name}</h3>
         <h4>Price: $ {price}</h4>
         <p>Manufacturer: {seller}</p>
-        <p>Rating: {ratings} star</p>
+        <p>Rating: {ratings} stars</p>
       </div>
-      <button className="btn-cart">Add to Cart</button>
+
+      <button onClick={() => handleAddToCart(props.product)} className="btn-cart">
+        Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -9,19 +9,41 @@ const Shop = () => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
-  }, []);
+    }, []);
+
+
+    
+    const [cart, setCart] = useState([])
+
+/*   product card এর মধ্যে button এ handleAddToCart add করতে..
+উল্লেখ্য যে, react state এর পরিবর্তনে বা onClick এর ক্ষেত্রে data সবসময় unidirectional মানে উপর থেকে নিচের effect বা পরিবর্তন ঘটাতে পারে
+অর্থ্যাৎ, যেমন, কোন 1টি button এ click করলে সচরাচর উক্ত component এর মধ্যে কোন একটা পরিবর্তন ঘটানো যায়।
+
+তবে, কখনও যদি এমন প্রয়োজন হয় যে, 1টি button এ click করলে তার effect তার উপরের অর্থ্যাৎ parent কোন component এ effect ঘটাবে, এমন প্রয়োজন হলে..
+সেক্ষেত্রে, onClick-টি -ই সরাসরি parent component এ তৈরি করে তাকে props ব্যবহার করে chicd component এ পাঠিয়ে দিয়ে তারপর ব্যবহার করলে.
+এক্ষেত্রে উক্ত onClick এর দ্বারা parent এর পরিবর্তন ঘটানো সম্ভব হবে।    */
+
+  const handleAddToCart = (product) => {
+    // console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
 
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-          <Product 
-          key={product.id} 
-          product={product}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+              ></Product>
         ))}
       </div>
       <div className="cart-container">
         <h4>Order Summary:</h4>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );
