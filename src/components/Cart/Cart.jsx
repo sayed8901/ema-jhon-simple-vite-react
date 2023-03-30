bdb0907feff6537965e0c996cefd6f5e966acf18
sayed8901 <sayed91515@gmail.com>
2023-03-30 13:22:56 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,20 +9,32 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
+
     for (const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+
+        // option 2.1: // যদি product এর quantity না থাকে মানে 0 হয়, তবে by default একে manually 1 করে দেওয়া..
+        // if(product.quantity === 0){
+        //     product.quantity = 1
+        // }
+
+        // option 2.2: // or can use shortcut if statement:
+        // product.quantity = product.quantity || 1;
+
+
+        quantity = quantity + product.quantity;
+
+        totalPrice = (totalPrice + product.price) * product.quantity;
+        totalShipping = (totalShipping + product.shipping) * product.quantity;
     }
 
-    const tax = totalPrice*10/100;
-
+    const tax = totalPrice * 10/100;
     const grandTotal = totalPrice + totalShipping + tax;
-
 
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -31,19 +31,19 @@ const Shop = () => {
       // step 2: get the 'product' from 'products' by using id
       const addedProduct = products.find(product => product.id === id)
       
-      // যেহেতু, প্রথমবার useEffect load নেওয়ার সময় 'addedProduct' empty array থাকে তাই, এখানে if condition দিয়ে বলে দেওয়া হয়েছে যে, যদি শুধুমাত্র 'addedProduct' এ কোন data থাকে মানে যদিি এটি emplty array না হয়, তবেই নিচের কাজগুলো হবে মানে quantity পাবে এবং quantity করতে পারবে।
+      // যেহেতু, প্রথমবার useEffect load নেওয়ার সময় 'addedProduct' empty array থাকে তাই, এখানে if condition দিয়ে বলে দেওয়া হয়েছে যে, যদি শুধুমাত্র 'addedProduct' এ কোন data থাকে মানে যদি এটি emplty array না হয়, তবেই নিচের কাজগুলো হবে মানে quantity পাবে এবং quantity করতে পারবে।
       if(addedProduct){
         // step 3.1: get quantity of the product saved in localStorage
         const quantity = storedCart[id];
-        console.log(id, quantity);
+        // console.log(id, quantity);
 
         // step 3.2: set quantity for "addedProduct"
         addedProduct.quantity = quantity;
-        console.log('added product:' ,addedProduct);
+        console.log('added product:', addedProduct);
 
         // step 4: add the 'addedProduct' to the 'savedCart'
         savedCart.push(addedProduct);
-        console.log(savedCart);
+        // console.log(savedCart);
       }
       // step 5: set the cart
       setCart(savedCart)
@@ -66,7 +66,37 @@ const Shop = () => {
 
   const handleAddToCart = (product) => {
     // console.log(product);
-    const newCart = [...cart, product];
+
+    let newCart = [];
+    // option 1: আগের 'cart' এর সাথে নতুন 'product' -কে add করে নতুনভাবে 'newCart' set করতে..
+    // (option 2.1 এবং 2.2 Cart.jsx এ আছে, এখানে মানে Shop.jsx এর option 1 active থাকলে পরের Cart.jsx এর option 2.1 বা 2.2 আর দরকার হবে না)
+
+    // const newCart = [...cart, product];
+
+    // option 2: নতুনভাবে 'newCart' set করার আগে product এর quantity (Cart.jsx এর বদলে) এখানেই check করে ফেলা..
+
+    
+    const exists = cart.find(pd => pd.id === product.id);
+    
+    // if product doesn't exists in the cart, then set quantity = 1 
+    // (প্রথমবার নির্দিষ্ট কোন product cart এ add করার ক্ষেত্রে)
+    if(!exists){
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }
+    
+    // if exists, update quantity every time by adding 1
+    else{
+      exists.quantity = exists.quantity + 1;
+
+      // কোন existing product পরে বারবার cart এ add করতে..
+      // এক্ষেত্রে, cart.find করে যেটা ‍already cart এ add করা আছে তা বাদে বাকিটা মানে remaining product বের করে 
+      const remaining = cart.filter(pd => pd.id !== product.id);
+
+      // newCart এ প্রথমে remaining product add করে তার সাথে already cart এ existing product -কে add করা..
+      newCart = [...remaining, exists];
+    }
+
     setCart(newCart);
 
     // to save data on localStorage
